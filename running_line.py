cb0f42ed570540d48370b1c0524298d9d31a260d
Alexey <alexeyz97@mail.ru>
2023-11-29 04:46:18 +0400

    Update running_line.py

--- running_line.py
+++ running_line.py
@@ -4,8 +4,6 @@ from os import path
 current_directory = os.path.dirname(os.path.realpath(__file__))
 
 def makeRuningLine(text, size=(100, 100), duration=3, textColor='white', backgroundColor=[0, 0, 0]):
-    
-
     textVideo = TextClip(text, color=textColor, fontsize=size[0] / 10,).set_duration(duration)
     textSize = textVideo.size
     textVideo = textVideo.set_position(lambda t: (size[0] - t * (size[0] + textSize[0]) / duration, (size[1] / 2) - (textSize[1] / 2)))
